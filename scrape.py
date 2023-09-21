0a3b37e4868aa4dfb74694c2999d18bb74a93a11
TSOlami <tijani.saheed@yahoo.com>
2023-09-21 12:33:31 +0100

    add type annotation, prompt and exception handling

--- scrape.py
+++ scrape.py
@@ -1,24 +1,39 @@
-# Import the necessary modules
 from requests_html import HTMLSession
 
-# Open a file in write mode to store the scraped HTML content
-file_path = 'scraped.html'
-html_file = open(file_path, 'w', encoding='utf-8')
 
-# Create an HTML session using requests_html library
-session = HTMLSession()
+# Prompt the user for a URL and file name
+url: str = input("Please enter a url: ")
+file_name: str = input(
+		"Please enter the file name to save the scraped content as: ")
 
-# Send a GET request to the specified URL
-r = session.get('https://www.naijaloaded.com.ng/download-music/wizkid-ft-tems-justin-bieber-essence-remix')
+def scraper()-> None:
+	# Open a file in append mode to store the scraped HTML content
+	file_path: str = f'{file_name}.html'
+	html_file: str = open(file_path, 'a', encoding='utf-8')
 
-# Get the HTML content of the response
-html_content = r.html.html
+	# Create an HTML session using requests_html library
+	session = HTMLSession()
 
-# Print the results on the terminal
-# print(html_content)
+	try:
+		# Send a GET request to the specified URL
+		r = session.get(url)
 
-# Write the HTML content to the file
-html_file.write(html_content)
+		# Get the HTML content of the response
+		html_content = r.html.html
 
-# Close the file after writing the HTML content
-html_file.close()
+		# Print the results on the terminal
+		# print(html_content)
+
+		# Write the HTML content to the file
+		html_file.write(html_content)
+
+		# Close the file after writing the HTML content
+		html_file.close()
+
+		print(f"Scraping completed. HTML content saved to '{file_name}.html'.")
+
+	except Exception as e:
+		print(f"An error occured: {e}")
+
+# Call the scraper function
+scraper()
