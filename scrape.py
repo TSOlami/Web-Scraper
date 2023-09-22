70411e21c9552a985e607e01306dc4edb931139d
TSOlami <tijani.saheed@yahoo.com>
2023-09-22 09:25:44 +0100

    add some cool print statements

--- scrape.py
+++ scrape.py
@@ -7,6 +7,9 @@ url: str = input("Please enter a url: ")
 file_name: str = input(
 		"Please enter the file name to save the scraped content as: ")
 
+# Print a message
+print("Your request is being processed, Please wait...")
+
 def scraper()-> None:
 	# Open a file in append mode to store the scraped HTML content
 	file_path: str = f'{file_name}.html'
@@ -18,15 +21,18 @@ def scraper()-> None:
 	try:
 		# Send a GET request to the specified URL
 		r = session.get(url)
+		print(".")
 
 		# Get the HTML content of the response
 		html_content = r.html.html
+		print(".")
 
 		# Print the results on the terminal
 		# print(html_content)
 
 		# Write the HTML content to the file
 		html_file.write(html_content)
+		print(".")
 
 		# Close the file after writing the HTML content
 		html_file.close()
